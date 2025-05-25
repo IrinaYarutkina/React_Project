fd442d69f6e21b44541b12da1b310541534f3eb6
Irina <irinaarutkina606@gmail.com>
2025-05-26 00:14:20 +0300

    Макет готов

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import Card from './Components/Card';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Card/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+        <footer> 
+            <p> Слова подготовлены </p>
+        </footer>
+    )
+}
+
+export default Footer
